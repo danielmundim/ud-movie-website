2fa81d805018c87d06686f9f141e0a8189446398
Daniel Mundim <dmundim.cr@gmail.com>
2017-01-20 12:56:11 -0300

    Fixing errors and adding pep8 compliance

--- media.py
+++ media.py
@@ -10,13 +10,17 @@ import webbrowser
 
 
 class Movie():
-"""
-Movie class used to instantiate movie objects to be used on creating the movies page
-"""
+    """
+    Movie class used to instantiate movie objects to be
+    used on creating the movies page
+    """
+
 
     def __init__(self, movie_title, movie_storyline, poster_image, url_trailer):
         """
-        Init function, runs once an instance is made, with all the correspondent parameters, sets the parameters passed through arguments to itself
+        Init function, runs once an instance is made, with all the correspondent
+        parameters, sets the parameters passed through arguments to itself
+    
         movie_title: title of the movie
         movie_storyline: description about the movie
         poster_image: url of poster image of the movie, to be shown on the website
@@ -26,9 +30,11 @@ Movie class used to instantiate movie objects to be used on creating the movies 
         self.storyline = movie_storyline
         self.poster_image_url = poster_image
         self.trailer_youtube_url = url_trailer
-
+    
+    
     def show_trailer(self):
         """
-        show_trailer: Opens the browser with the URL informed on it's "trailer" property
+        show_trailer: Opens the browser with the URL informed
+        on it's "trailer" property
         """
         webbrowser.open(self.trailer)
